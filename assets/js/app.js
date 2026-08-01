c78bb961a3b3c6fa8212fe13b569bba38b68481f
PrashantSinghRajputOfficial <afeemlifestyle@gmail.com>
2026-08-01 13:04:02 +0530

    Completely crop out YouTube native overlay controls and titles using precision video croppers

--- assets/js/app.js
+++ assets/js/app.js
@@ -974,7 +974,9 @@ function initShoppableReelsCarousel() {
         return `
             <div class="reel-item-card" onclick="openReelModal(${realDataIndex})">
                 <div class="reel-video-box">
-                    <iframe class="reel-card-video" src="https://www.youtube-nocookie.com/embed/${reel.youtubeId}?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&showinfo=0&iv_load_policy=3&disablekb=1&loop=1&playlist=${reel.youtubeId}&playsinline=1" title="${reel.title}" frameborder="0" allow="autoplay; encrypted-media"></iframe>
+                    <div class="reel-iframe-cropper">
+                        <iframe class="reel-card-video" src="https://www.youtube-nocookie.com/embed/${reel.youtubeId}?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&showinfo=0&iv_load_policy=3&disablekb=1&loop=1&playlist=${reel.youtubeId}&playsinline=1" title="${reel.title}" frameborder="0" allow="autoplay; encrypted-media"></iframe>
+                    </div>
                     <div class="reel-video-touch-shield"></div>
                     <div class="reel-badge-thumb">
                         <img src="${reel.img}" alt="${reel.title}">
@@ -1022,7 +1024,9 @@ window.openReelModal = function(index) {
 
     if (modal && videoFrameContainer) {
         videoFrameContainer.innerHTML = `
-            <iframe id="reel-modal-video-iframe" class="reel-modal-video" src="https://www.youtube-nocookie.com/embed/${reel.youtubeId}?autoplay=1&mute=0&controls=0&modestbranding=1&rel=0&showinfo=0&iv_load_policy=3&disablekb=1&loop=1&playlist=${reel.youtubeId}&playsinline=1&enablejsapi=1" title="${reel.title}" frameborder="0" allow="autoplay; encrypted-media"></iframe>
+            <div class="reel-modal-video-cropper">
+                <iframe id="reel-modal-video-iframe" class="reel-modal-video" src="https://www.youtube-nocookie.com/embed/${reel.youtubeId}?autoplay=1&mute=0&controls=0&modestbranding=1&rel=0&showinfo=0&iv_load_policy=3&disablekb=1&loop=1&playlist=${reel.youtubeId}&playsinline=1&enablejsapi=1" title="${reel.title}" frameborder="0" allow="autoplay; encrypted-media"></iframe>
+            </div>
             <div class="reel-video-touch-shield"></div>
         `;
 
